c98056cee2ceaec02468283b010c6b2cfc5cb943
Joao Coelho <joaodscoelho@gmail.com>
2025-03-01 22:53:03 +0000

    Adds new stop words and table into index view and removes empty words

--- wordCloudApp/routes/index.js
+++ wordCloudApp/routes/index.js
@@ -3,10 +3,8 @@ const axios = require('axios');
 const constants = require('./../constants');
 const router = express.Router();
 
-async function fetchEndpoints() {
+async function fetchEndpointsData(endpointURLs) {
   try {
-    const endpointURLs = Array.from({ length: 3 }).map((_, index) => `https://dummyjson.com/posts/${index + 1}`);
-
     return await Promise.all(endpointURLs.map(url => axios.get(url)));
   } catch (error) {
     console.error('Error fetching endpoints:', error.message);
@@ -14,34 +12,40 @@ async function fetchEndpoints() {
   }
 }
 
-async function wordWork() {
-  const endpointsData = await fetchEndpoints();
+async function wordWork(endpointsData) {
   const uniqueWords = [];
   const resultArray = [];
 
   endpointsData.forEach(eData => {
     eData.data.body.split(' ').map(word => {
-      const wordAux = word.replace(/[^a-zA-Z ]/g, '').toLowerCase();
+      const wordAux = word.replace(/[^a-zA-Z]/g, '').toLowerCase().trim();
       
-      if (!constants.STOP_WORDS.includes(wordAux)) {
+      if (wordAux && !constants.STOP_WORDS.includes(wordAux)) {
         if (!uniqueWords.includes(wordAux)) {
-          resultArray.push({ word: wordAux, count: 1 });
+          resultArray.push({ 
+            word: wordAux,
+            abs_frequency: 1,
+            rel_frequency: 0
+          });
           uniqueWords.push(wordAux);
         } else {
-          resultArray.find(o => o.word === wordAux).count++;
+          resultArray.find(o => o.word === wordAux).abs_frequency++;
         }
       }
     });
   });
-  // removes words with just 1 count and sort by count in descending order
+  // removes words with just 1 abs_frequency and sort by abs_frequency in descending order
   return resultArray
-    .filter(o => o.count > 1)
-    .sort((a, b) => b.count - a.count);
+    .filter(o => o.abs_frequency > 1)
+    .sort((a, b) => b.abs_frequency - a.abs_frequency);
 }
 
 router.get('/', async (_, res) => {
-  const results = await wordWork();
-  res.render('index', { results });
+  const endpointURLs = Array.from({ length: 100 }).map((_, index) => `https://dummyjson.com/posts/${index + 1}`);
+  const endpointsData = await fetchEndpointsData(endpointURLs);
+  const wordResults = await wordWork(endpointsData);
+
+  res.render('index', { wordResults });
 });
 
 module.exports = router;
